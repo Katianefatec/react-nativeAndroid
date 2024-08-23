621af7a1cb302adf8a7e78058dee16819f3a6e07
Katiane <katiane.soares@fatec.sp.gov.br>
2024-08-23 18:57:00 -0300

    testando configuracoes

---
+++ component/separator/index.tsx
@@ -0,0 +1,10 @@
+import { View } from "react-native"
+import style from "./style"
+
+const Separator = () => {
+    return (
+        <View style={style.separator} />
+    )
+}
+
+export default Separator
